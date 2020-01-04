9cf7eb8e71b040e7103ac64094596d15184b6ee9
Rod-X <913934542@qq.com>
2020-01-04 10:18:31 +0800

    添加axios，发送请求首页轮播图，并配置baseurl与axios拦截

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,13 @@
 import React, { Component,Fragment } from 'react'
+import Axios from '../../utils/request'
 
 export default class index extends Component {
+    componentDidMount(){
+        Axios.get("/home/swiper").then((res)=>{
+            console.log(res);
+        })
+    }
+
     render() {
         return (
             <Fragment>
