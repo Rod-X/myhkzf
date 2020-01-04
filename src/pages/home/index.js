4782ff192cd60fe579c25ded669330d4d1bf0dad
Rod-X <913934542@qq.com>
2020-01-04 10:33:16 +0800

    新分支dev

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,6 +2,8 @@ import React, { Component,Fragment } from 'react'
 import Axios from '../../utils/request'
 
 export default class index extends Component {
+
+    
     componentDidMount(){
         Axios.get("/home/swiper").then((res)=>{
             console.log(res);
